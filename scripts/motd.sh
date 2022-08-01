bd8bd209ab0e4a7612e14fcc287d536fe157a5f5
Debdut Chakraborty <debdut.chakraborty@rocket.chat>
2022-08-01 13:02:42 +0530

    chore: update motd to docker backend

--- scripts/motd.sh
+++ scripts/motd.sh
@@ -4,14 +4,9 @@ printf "########################################################################
 Rocket.Chat is the leading open source team chat software solution. Free, unlimited and completely customizable with on-premises and SaaS cloud hosting.
 Replace email, HipChat & Slack with the ultimate team chat software solution.
 
-To configure your Rocket.Chat service and traefik loadbalancer with your public domain (ROOT_URL) run:
+This installation uses docker as its backend. For more information about our docker usage read here: https://docs.rocket.chat/quick-start/deploying-rocket.chat/rapid-deployment-methods/docker-and-docker-compose
 
-  rocketchatctl configure --lets-encrypt --root-url=ROOT_URL --letsencrypt-email=EMAIL
-  example: # rocketchatctl configure --lets-encrypt --root-url=https://myrocketchatserver.org --letsencrypt-email=myemail@example.org
-  
-Keep your RocketChat server updated using rocketchatctl update. Run rocketchatctl -h to see the full list of available options.
-  
-In case you do not own a public domain, you could use the public IP of your droplet, but traefik will not be able to fetch certificates for you so you will see a privacy alert message when loading https://droplet-IP
+You can find the docker templates in your $HOME/rocketchat.
  
 Looking for how to use Rocket.Chat? Be sure to check our docs: https://rocket.chat/docs
 Need some help? Join our community forums https://forums.rocket.chat
