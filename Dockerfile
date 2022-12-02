a8ec3294285d8b0cc2f6ace36dd427a4126c3db3
Debdut Chakraborty <debdut.chakraborty@rocket.chat>
2022-12-02 07:21:35 +0530

    Move to composite action

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM hashicorp/packer:1.7.2
-
-RUN apk add --no-cache sudo curl wget openssh jq
-
-COPY . /deploy
-
-WORKDIR /deploy
-
-ENTRYPOINT ["bash", "/deploy/entrypoint.sh"]
--- scripts/extra.sh
+++ scripts/extra.sh
@@ -2,3 +2,6 @@
 
 wget -O- https://raw.githubusercontent.com/digitalocean/marketplace-partners/master/scripts/90-cleanup.sh | sudo bash
 wget -O- https://raw.githubusercontent.com/digitalocean/marketplace-partners/master/scripts/99-img-check.sh | sudo bash
+
+sudo rm -rf /home/ubuntu/.ssh
+sudo rm -rf /root/.ssh
